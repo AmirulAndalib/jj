b33517c2ea263b67c934b5165ed552ea03d43ccd
Remo Senekowitsch <remo@buenzli.dev>
2025-11-20 22:22:25 +0100

    docs: copy bulk of content from mkdoc to astro
    Lots of stuff still broken and missing.

--- docs-new/astro.config.mjs
+++ docs-new/astro.config.mjs
@@ -31,17 +31,101 @@ export default defineConfig({
 			],
 			credits: true, // TODO: remove? I personally like this info as a site visitor.
 			sidebar: [
+				{ label: "Home", slug: "index" },
+				{
+					label: "Getting started",
+					items: [
+						{ label: "Installation and setup", slug: "install-and-setup" },
+						{ label: "Tutorial and bird's eye view", slug: "tutorial" },
+						{ label: "Working with Gerrit", slug: "gerrit" },
+						{ label: "Working with GitHub", slug: "github" },
+						{ label: "Working on Windows", slug: "windows" },
+					],
+				},
+				{ label: "FAQ", slug: "faq" },
+				{ label: "CLI reference", slug: "cli-reference" },
+				{ label: "Testimonials", slug: "testimonials" },
+				{ label: "Community-built tools", slug: "community_tools" },
+				{
+					label: "Concepts",
+					items: [
+						{ label: "Working copy", slug: "working-copy" },
+						{ label: "Bookmarks", slug: "bookmarks" },
+						{ label: "Conflicts", slug: "conflicts" },
+						{ label: "Operation log", slug: "operation-log" },
+						{ label: "Glossary", slug: "glossary" },
+					],
+				},
 				{
 					label: "Guides",
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: "Example Guide", slug: "guides/example" },
+						{ label: "Divergent changes", slug: "guides/divergence" },
+						{ label: "Multiple remotes", slug: "guides/multiple-remotes" },
 					],
 				},
 				{
 					label: "Reference",
-					autogenerate: { directory: "reference" },
+					items: [
+						{ label: "Settings", slug: "config" },
+						{ label: "Fileset language", slug: "filesets" },
+						{ label: "Revset language", slug: "revsets" },
+						{ label: "Templating language", slug: "templates" },
+					],
 				},
+				{
+					label: "Comparisons",
+					items: [
+						{ label: "Git comparison", slug: "git-comparison" },
+						{ label: "Git command table", slug: "git-command-table" },
+						{ label: "Git compatibility", slug: "git-compatibility" },
+						{ label: "Sapling comparison", slug: "sapling-comparison" },
+						{ label: "Other related work", slug: "related-work" },
+					],
+				},
+				{
+					label: "Technical details",
+					items: [
+						{ label: "Core tenets", slug: "core_tenets" },
+						{ label: "Architecture", slug: "technical/architecture" },
+						{ label: "Concurrency", slug: "technical/concurrency" },
+						{ label: "Conflicts", slug: "technical/conflicts" },
+					],
+				},
+				{
+					label: "Contributing",
+					items: [
+						{ label: 'Guidelines and "How to...?"', slug: "contributing" },
+						{ label: "Code of conduct", slug: "code-of-conduct" },
+						{ label: "Style guide", slug: "style_guide" },
+						{ label: "Design docs", slug: "design_docs" },
+						{ label: "Design doc blueprint", slug: "design_doc_blueprint" },
+						{ label: "Releasing", slug: "releasing" },
+						{
+							label: "Temporary voting for governance",
+							slug: "governance/temporary-voting",
+						},
+						// { label: "Governance", slug: "governance/GOVERNANCE" },
+					],
+				},
+				{
+					label: "Design docs",
+					items: [
+						{ label: "git-submodules", slug: "design/git-submodules" },
+						{
+							label: "git-submodule-storage",
+							slug: "design/git-submodule-storage",
+						},
+						{ label: "JJ run", slug: "design/run" },
+						{ label: "Sparse patterns v2", slug: "design/sparse-v2" },
+						{ label: "Tracking branches", slug: "design/tracking-branches" },
+						{
+							label: "Copy tracking and tracing",
+							slug: "design/copy-tracking",
+						},
+					],
+				},
+				{ label: "Development roadmap", slug: "roadmap" },
+				{ label: "Changelog", slug: "changelog" },
 			],
 		}),
 	],
